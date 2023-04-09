5a3fc0411b063b55401ef7acef7b1648f06cb37b
Ryan Lo <lowc1012@gmail.com>
2023-04-09 16:05:03 +0000

    create echo server

---
+++ server.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	server := echo.New()
+	server.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, Echo!")
+	})
+
+	server.Logger.Fatal(server.Start(":8080"))
+}
